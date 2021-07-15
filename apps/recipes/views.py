574513df2b91e41bb84503a999ae6e249a167028
ВашÐÐÐDenis Kudrik <deniskudrik@gmail.com>
2021-07-15 16:23:39 +0300

    Fix subscription system

--- apps/recipes/views.py
+++ apps/recipes/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.http.response import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views.decorators.http import require_http_methods
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
