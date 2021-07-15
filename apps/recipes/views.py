ef1507667fbefc086375f34e4a493716a4db6436
ВашÐÐÐDenis Kudrik <deniskudrik@gmail.com>
2021-07-15 16:15:16 +0300

    Fix subscription system

--- apps/recipes/views.py
+++ apps/recipes/views.py
@@ -107,21 +107,12 @@ def profile(request, username):
 @login_required
 @api_view(['POST', 'DELETE'])
 def subscriptions(request):
-    print('FUNC SUBSCRIPTIONS')
-    if request.method == 'POST':
-        author_id = request.data.get('id')
-        author = get_object_or_404(User, id=author_id)
-        if request.user == author:
-            return JsonResponse({'success': False})
-        Follow.objects.get_or_create(user=request.user, author=author)
-        return JsonResponse({'success': True})
-    elif request.method == 'DELETE':
-        author_id = request.data.get('id')
-        author = get_object_or_404(User, id=author_id)
-        follow_to_delete = Follow.objects.filter(user=request.user,
-                                                 author=author)
-        follow_to_delete.delete()
-        return JsonResponse({'success': True})
+    author_id = request.data.get('id')
+    author = get_object_or_404(User, id=author_id)
+    if request.user == author:
+        return JsonResponse({'success': False})
+    Follow.objects.get_or_create(user=request.user, author=author)
+    return JsonResponse({'success': True})
 
 
 @login_required
