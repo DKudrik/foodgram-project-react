a6a91ba3f8a1ff840c03afe6dc807bcaa3c580b4
ВашÐÐÐDenis Kudrik <deniskudrik@gmail.com>
2021-07-18 17:02:49 +0300

    Add console logging

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -142,3 +142,18 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
     }
 }
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
