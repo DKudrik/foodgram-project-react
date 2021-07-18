05053f1defd4debaff4dd1be21c365cde61a6402
ВашÐÐÐDenis Kudrik <deniskudrik@gmail.com>
2021-07-18 14:36:02 +0300

    Fix subscriptions 16

--- assets/js/api/Api.js
+++ assets/js/api/Api.js
@@ -70,7 +70,7 @@ class Api {
       })
   }
   removeSubscriptions (id) {
-    return fetch(`/remove_subscription/${id}/`, {
+    return fetch(`/subscriptions/${id}/`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
