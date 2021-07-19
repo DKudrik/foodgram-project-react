6d135d0da9c62ccf2c855fddaa6f57756decd285
ВашÐÐÐDenis Kudrik <deniskudrik@gmail.com>
2021-07-19 10:10:35 +0300

    Fix subscriptions

--- apps/recipes/views.py
+++ apps/recipes/views.py
@@ -124,9 +124,9 @@ def subscriptions(request):
 
 
 @login_required
-@require_http_methods(['DELETE'])
+@api_view(['DELETE'])
 def remove_subscription(request, author_id):
-    author_id = author_id
+    author_id = request.data.get('id')
     author = get_object_or_404(User, id=author_id)
     follow_to_delete = Follow.objects.filter(user=request.user,
                                              author=author)
